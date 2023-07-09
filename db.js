d8ea335ff2620d062334f77af1ed1eab2b6e9525
Jignesh <jigsvadiyatar6557@gmail.com>
2023-07-09 11:26:42 +0530

    add

--- db.js
+++ db.js
@@ -5,7 +5,3 @@ require("dotenv").config()
 const connection=mongoose.connect(process.env.mongoURL)
 
 module.exports={connection}
-
-// API_KEY = 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
-// port = 8080
-// mongoURL = mongodb+srv://krunalgurao:krunalgurao@digitron.zojf4xp.mongodb.net/Digitron?retryWrites=true&w=majority
